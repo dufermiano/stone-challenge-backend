175aca06bdb42e99a81343c41e151f8a73cf7c8b
Eduardo Fermiano Rocha <Eduardo.Rocha@sky.com.br>
2021-01-24 04:23:23 -0300

    implementando prévia JWT

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import serverless from 'serverless-http';
 import express from 'express';
 import mainRouter from './src/routes/mainRouter';
+import bodyParser from 'body-parser';
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(mainRouter);
 
 exports.handler = serverless(app);
--- src/routes/mainRouter.js
+++ src/routes/mainRouter.js
@@ -1,9 +1,46 @@
 import { Router } from 'express';
 import { hello, hello2 } from '../lambdas';
+import jwt from 'jsonwebtoken';
+
+function verifyJWT(req, res, next) {
+  const token = req.headers['x-access-token'];
+  if (!token)
+    return res.status(401).json({ auth: false, message: 'No token provided.' });
+
+  jwt.verify(token, 'a', function (err, decoded) {
+    if (err)
+      return res
+        .status(500)
+        .json({ auth: false, message: 'Failed to authenticate token.' });
+
+    // se tudo estiver ok, salva no request para uso posterior
+    req.userId = decoded.id;
+    next();
+  });
+}
 
 const mainRouter = Router();
 
-mainRouter.get('/hello', hello);
+mainRouter.get('/hello', verifyJWT, hello);
 mainRouter.get('/hello2', hello2);
+
+mainRouter.post('/login', (req, res, next) => {
+  //esse teste abaixo deve ser feito no seu banco de dados
+  if (req.body.user === 'edu' && req.body.password === '123') {
+    console.log(JSON.stringify(req.body));
+    //auth ok
+    const id = 1; //esse id viria do banco de dados
+    const token = jwt.sign({ id }, 'a', {
+      expiresIn: 300, // expires in 5min
+    });
+    return res.json({ auth: true, token: token });
+  }
+
+  res.status(500).json({ message: 'Login inválido!' });
+});
+
+mainRouter.post('/logout', function (req, res) {
+  res.json({ auth: false, token: null });
+});
 
 export default mainRouter;
