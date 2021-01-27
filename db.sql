51b2ca180d1b925ec93da0352d706368b0510659
Eduardo Fermiano Rocha <Eduardo.Rocha@sky.com.br>
2021-01-27 20:13:56 -0300

    colocando db na AWS

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE STONE;
+CREATE DATABASE MARVELAPP;
 
-USE STONE;
+USE MARVELAPP;
 
 CREATE TABLE USER(userId int auto_increment primary key, 
 	username varchar(50) NOT NULL UNIQUE, password varchar(15) NOT NULL, email varchar(50) NOT NULL,
@@ -9,4 +9,4 @@ CREATE TABLE USER(userId int auto_increment primary key,
 CREATE TABLE FAVORITES (favId int auto_increment primary key, characterId int(10), comicId int(10),
 userId int,
 CONSTRAINT FK_UserFav FOREIGN KEY (userId)
-    REFERENCES User(userId));
+    REFERENCES USER(userId));
--- src/services/db.js
+++ src/services/db.js
@@ -3,10 +3,10 @@ import mysql from 'mysql2/promise';
 async function connectDB() {
   try {
     const connection = await mysql.createConnection({
-      user: 'root',
-      password: 'rootroot',
-      host: 'localhost',
-      database: 'STONE',
+      user: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      host: process.env.DB_HOST,
+      database: process.env.DB_DATABASE,
     });
 
     console.log('Conectou no MySQL!');
